6eaf9461a4d207718e488af5adbccbd99251e253
Neha Kumari <110078405+nehatkr@users.noreply.github.com>
2025-07-11 13:38:08 +0530

    Updated landing.jsx

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -21,7 +21,7 @@ const LandingPage = () => {
     <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20 relative">
       <section className="text-center fade-in">
         <h1 className="flex flex-col items-center justify-center gradient-title text-4xl font-extrabold sm:text-6xl lg:text-8xl tracking-tighter py-4 animate-pulse-slow">
-          Find Your Dream Job and get{" "}
+          Find Your Dream Job{" "}
           <span className="flex items-center gap-2 sm:gap-6 slide-up stagger-1">
             and get{" "}
             <img
